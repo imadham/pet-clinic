e27ec271b0f32b4f4e71a70d23ca2a5d9ebe6e09
imadham <imad.hamameh@gmail.com>
2020-04-28 13:09:08 +0300

    create Owner index page and controller. Closes #13.

--- pet-clinic-web/src/main/java/com/imad/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/imad/petclinic/controllers/OwnerController.java
@@ -2,12 +2,11 @@ package com.imad.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
-public class VetController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+public class OwnerController {
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners(){
 
-        return "vets/index";
+        return "owners/index";
     }
 }
