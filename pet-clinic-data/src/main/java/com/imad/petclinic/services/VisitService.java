03c3047212ef8c9d7cbed7d3026b77a7aece8270
imadham <imad.hamameh@gmail.com>
2020-06-16 04:21:05 +0300

    Create service for visit (spring data jpa and map service) Closes #47.

---
+++ pet-clinic-data/src/main/java/com/imad/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.imad.petclinic.services;
+
+import com.imad.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
--- pet-clinic-web/src/main/java/com/imad/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/imad/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.imad.petclinic.bootstrap;
 
 import com.imad.petclinic.model.*;
-import com.imad.petclinic.services.OwnerService;
-import com.imad.petclinic.services.PetTypeService;
-import com.imad.petclinic.services.SpecialtyService;
-import com.imad.petclinic.services.VetService;
+import com.imad.petclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -90,6 +90,12 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(aliPet);
 
         ownerService.save(owner2);
+
+        Visit dogVisit = new Visit();
+        dogVisit.setPet(aliPet);
+        dogVisit.setDate(LocalDate.now());
+        dogVisit.setDescription("ill dog");
+        visitService.save(dogVisit);
 
         System.out.println("Loading owners...");
 
