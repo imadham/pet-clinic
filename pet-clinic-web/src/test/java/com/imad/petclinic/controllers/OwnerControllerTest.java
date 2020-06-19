19e2abd4a780d1fe1bf5f3f5c8e7eab380634e73
imadham <imad.hamameh@gmail.com>
2020-06-19 06:01:11 +0300

    circle CI test

--- pet-clinic-web/src/test/java/com/imad/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/imad/petclinic/controllers/OwnerControllerTest.java
@@ -19,6 +19,7 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//ds
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
