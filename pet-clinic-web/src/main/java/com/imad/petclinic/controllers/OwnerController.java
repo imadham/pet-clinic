e58933643969e1e6165a287ac9e1468d389e75df
imadham <imad.hamameh@gmail.com>
2020-06-28 03:01:52 +0300

    Display owner record(HTML), closes #53

--- pet-clinic-web/src/main/java/com/imad/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/imad/petclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package com.imad.petclinic.controllers;
 
+import com.imad.petclinic.model.Owner;
 import com.imad.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +29,13 @@ public class OwnerController {
     @RequestMapping({"/find", "/find.html"})
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 }
